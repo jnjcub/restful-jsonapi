8b9101c6752cd21f8d21feaade16beb2cb1e3e6b
Jayan Jacob <jjacob@netflix.com>
2016-09-27 22:36:23 -0700

    return source and detail

--- lib/restful/jsonapi/serializable_errors.rb
+++ lib/restful/jsonapi/serializable_errors.rb
@@ -3,12 +3,17 @@ module Restful
     module SerializableErrors
       extend ActiveSupport::Concern
 
-      def serializable_errors(object)
-        prefix = object.class.to_s.demodulize.underscore
+      def serializable_errors(object, options={})
+        #prefix = object.class.to_s.demodulize.underscore
 
         errors = object.errors.to_hash.each_with_object([]) do |(k, v), array|
           v.each do |msg|
-            array.push(id: "#{prefix}.#{k}", title: msg)
+            translated_key = "#{k.to_s.gsub('_', '-')}"
+            array.push(
+                       title: msg,
+                       detail: "#{k.to_s.camelize} #{msg}",
+                       source: { pointer: "/data/attributes/#{translated_key}"}
+                      )
           end
         end
 
